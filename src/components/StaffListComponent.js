774b11f6354d9a14157599851b6d4e1b04c5b7ff
Curtis <nghiatintruong@gmail.com>
2022-07-09 00:13:59 +0700

    nineth-times updates

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -83,6 +83,17 @@ class Staffs extends Component {
     );
   }
 
+  render6Cols() {
+    return (
+      document
+        .querySelectorAll(".staffs")
+        .forEach((staff) => staff.classList.remove("col-lg-4", "col-lg-6")),
+      document
+        .querySelectorAll(".staffs")
+        .forEach((staff) => staff.classList.add("col-lg-2"))
+    );
+  }
+
   render() {
     const list = this.props.staffs.map((staff) => {
       return (
