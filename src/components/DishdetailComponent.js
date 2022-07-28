82805c9f34b5f51c01a23f1afc0d58a852b96d77
Curtis <nghiatintruong@gmail.com>
2022-07-28 12:01:42 +0700

    Exercise 3

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import {
   Card,
   CardImg,
@@ -7,6 +7,15 @@ import {
   CardTitle,
   Breadcrumb,
   BreadcrumbItem,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  FormGroup,
+  Form,
+  Label,
+  Input,
+  FormFeedback,
 } from "reactstrap";
 import { Link } from "react-router-dom";
 function RenderDish({ dish }) {
@@ -47,10 +56,117 @@ function RenderComments({ comments }) {
             );
           })}
         </ul>
+        <CommentForm />
       </div>
     );
   } else {
     return <div></div>;
+  }
+}
+
+class CommentForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      yourname: "",
+      isModalOpen: false,
+      touched: {
+        yourname: false,
+      },
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+    });
+  }
+
+  handleBlur = (field) => (evt) => {
+    this.setState({
+      touched: { ...this.state.touched, [field]: true },
+    });
+  };
+
+  validate(yourname) {
+    const errors = {
+      yourname: "",
+    };
+
+    if (this.state.touched.yourname && yourname.length < 2)
+      errors.yourname = "Must be greater than 2 characters";
+    else if (this.state.touched.yourname && yourname.length >= 15)
+      errors.yourname = "Must be 15 characters or less";
+    return errors;
+  }
+
+  render() {
+    const errors = this.validate(this.state.yourname);
+    return (
+      <div>
+        <Button outline onClick={this.toggleModal}>
+          <span className="fa fa-pencil fa-lg"></span> Submit Comment
+        </Button>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+          <ModalBody>
+            <Form>
+              <FormGroup>
+                <Label htmlFor="rating" className="font-weight-bold">
+                  Rating
+                </Label>
+                <Input type="select" id="rating" name="rating">
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </Input>
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="yourname" className="font-weight-bold">
+                  Your Name
+                </Label>
+                <Input
+                  type="text"
+                  id="yourname"
+                  name="yourname"
+                  placeholder="Your name"
+                  value={this.state.yourname}
+                  valid={errors.yourname === ""}
+                  invalid={errors.yourname !== ""}
+                  onBlur={this.handleBlur("yourname")}
+                  onChange={this.handleInputChange}
+                />
+                <FormFeedback>{errors.yourname}</FormFeedback>
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="comment" className="font-weight-bold">
+                  Comment
+                </Label>
+                <Input type="textarea" id="comment" rows="6" name="comment" />
+              </FormGroup>
+              <Button type="submit" color="primary">
+                Submit
+              </Button>
+            </Form>
+          </ModalBody>
+        </Modal>
+      </div>
+    );
   }
 }
 
@@ -59,6 +175,9 @@ const DishDetail = (props) => {
     <div className="container">
       <div className="row">
         <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/home">Home</Link>
+          </BreadcrumbItem>
           <BreadcrumbItem>
             <Link to="/menu">Menu</Link>
           </BreadcrumbItem>
